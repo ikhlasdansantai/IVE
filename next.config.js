c43e62750941215d50c9f69e26120b5e755eecbd
ikhlasdansantai <initesting5@gmail.com>
2023-10-29 21:14:36 +0700

    first commit || config done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    // formats: ["image/avif", "image/webp"],
+    domains: ["i.ytimg.com", "i.pinimg.com", "media.giphy.com"], // Tambahkan host i.ytimg.com ke dalam array
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
