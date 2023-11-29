ecb8215a8b06f118ceb02670c89054ede5dda817
ikhlasdansantai <initesting5@gmail.com>
2023-11-29 20:02:10 +0700

    menambahkan firebase

---
+++ src/lib/firebase/init.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCbB6xfrt1LRk3B3arpovSxC9Fp7N0fsaE",
+  authDomain: "ive-firebase-fffcb.firebaseapp.com",
+  projectId: "ive-firebase-fffcb",
+  storageBucket: "ive-firebase-fffcb.appspot.com",
+  messagingSenderId: "1050977630570",
+  appId: "1:1050977630570:web:1bfc4481442769d370cd13",
+  measurementId: "G-GKCJ08BY0R",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/lib/firebase/store.ts
@@ -0,0 +1,19 @@
+import { getDoc, getDocs, doc, collection, getFirestore } from "firebase/firestore";
+import app from "./init";
+
+const firestore = getFirestore(app);
+
+export async function retreiveData(collectionName: string) {
+  const snapshot = await getDocs(collection(firestore, collectionName));
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return data;
+}
+
+export async function retreiveDataById(collectionName: string, id: string) {
+  const snapshot = await getDoc(doc(firestore, collectionName, id));
+  const data = snapshot.data();
+  return data;
+}
