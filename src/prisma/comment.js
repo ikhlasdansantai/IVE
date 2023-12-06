265abfd802caddd8225130819b9b6bb8aa5eff90
cobateknologi <mikhsannuryaman@gmail.com>
2023-12-06 19:54:42 +0700

    folder untuk prisma yagesya

---
+++ src/prisma/comment.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const createComment = async (title, author, tags) => {
+  const comment = await prisma.comment.create({
+    data: {
+      title,
+      author,
+      tags,
+    },
+  });
+
+  return comment;
+};
+
+export const getAllComment = async () => {
+  const comments = await prisma.comment.findMany();
+
+  return comments;
+};
+
+export const deleteComment = async (id) => {
+  const comment = await prisma.comment.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  return comment;
+};
