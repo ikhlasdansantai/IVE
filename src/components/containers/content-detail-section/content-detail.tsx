a687e5663669b6c49efcdbe34e7c95f4108dbb7b
ikhlasdansantai <initesting5@gmail.com>
2024-06-08 08:44:09 +0700

    refactor: this commit will refactor all component code

--- src/components/containers/content-detail-section/content-detail.tsx
+++ src/components/containers/content-detail-section/content-detail.tsx
@@ -1,28 +1,31 @@
-"use client";
+// "use client";
+import TimeAgo from "@/components/common/convertTime/convertTime";
+// import { useState } from "react";
 
-import { useState } from "react";
-
-export default function ContentDetail({ title, desc }: any) {
-  const [showContent, setShowContent] = useState(false);
+export default function ContentDetail({ title, desc, publishedAt }: any) {
+  // const [showContent, setShowContent] = useState(false);
   return (
     <div className="content__detail mt-3 md:mt-8">
       <h1 className="font-semibold text-[#121212] md:text-3xl">{title}</h1>
-      <span className="mt-2 block text-xs">10 Hari Yang Lalu</span>
+      <span className="mt-2 block text-xs">
+        <TimeAgo getDate={publishedAt} />
+      </span>
     </div>
   );
 }
 
-function ShowContentDetail({ setState, desc }: any) {
-  return (
-    <div className="mt-6">
-      <p>{desc}</p>
+// MAYBE unused code
+// function ShowContentDetail({ setState, desc }: any) {
+//   return (
+//     <div className="mt-6">
+//       <p>{desc}</p>
 
-      <button
-        onClick={() => setState(false)}
-        className="mt-4 font-semibold text-gray-700 underline"
-      >
-        Show Less
-      </button>
-    </div>
-  );
-}
+//       <button
+//         onClick={() => setState(false)}
+//         className="mt-4 font-semibold text-gray-700 underline"
+//       >
+//         Show Less
+//       </button>
+//     </div>
+//   );
+// }
